085e254e72a2b470de3ffdbf4260a77b37d18b9b
ChyntiaTambunan <chyntiatambunan14@gmail.com>
2026-04-07 21:40:44 +0700

    chyntiaa
    y

--- t02_01.c
+++ t02_01.c
@@ -1,12 +1,45 @@
-// NIM - Name
-// NIM - Name
+// 12S25015 - Chyntia Lovenia Tambunan
 
 #include <stdio.h>
 
-int main(int _argv, char **_argc)
-{
-  //code
-  
-  return 0;
+struct perkalian {
+  float jumlah;
+  float harga;
+  float hasil;
+  float diskon;
+  float akhir;
+};
 
+int main(int _argv, char **_argc){
+  struct perkalian a;
+
+  scanf("%f", &a.jumlah);
+  scanf("%f", &a.harga);
+
+  a.hasil = a.jumlah * a.harga;
+  
+  if (a.hasil > 500000){
+    a.diskon = a.hasil * 0.15;
+    a.akhir = a.hasil - a.diskon;
+    printf("%.2f\n", a.diskon);
+  }
+  else if (a.hasil > 100000){
+    a.diskon = a.hasil * 0.10;
+    a.akhir = a.hasil - a.diskon;
+    printf("%.2f\n", a.diskon);
+  }
+  else if(a.hasil >= 50000){
+    a.diskon = a.hasil * 0.05;
+    a.akhir = a.hasil - a.diskon;
+    printf("%.2f\n", a.diskon);
+  }
+  else{
+    a.diskon = 0;
+    printf("---\n");
+    a.akhir = a.hasil;
+  }
+  
+  printf("%.2f\n", a.akhir);
+
+  return 0;
 }
--- t02_02.c
+++ t02_02.c
@@ -1,8 +1,12 @@
-// NIM - Name
-// NIM - Name
+// 12S25015 // Chyntia Lovenia Tambunan
 
 #include <stdio.h>
 
+struct makanan{
+  char nama[20];
+  char menu[100];
+  
+}
 int main(int _argv, char **_argc)
 {
   //code
